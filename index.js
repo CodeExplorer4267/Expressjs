819074c36c08ef66c41d538b97619d8aa1ba8454
CodeExplorer4267 <rupambhadra478@gmail.com>
2024-10-13 17:15:52 +0530

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 const app = express()
 const port=3000;
 
+app.set("view engine","ejs")
+
 app.use(function(req,res,next){
   next()
 })
@@ -9,9 +11,16 @@ app.use(function(req,res,next){
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.get('/profile', function (req, res) {
+  res.render("profile")
+})
+
+app.get('/profile/:username',(req,res)=>{//zekono username dite pari
+  res.send(`This is profile of:${req.params.username}`)//seta ekahne dekhabe
+})
 
 app.get('/contact',(req,res)=>{
-  res.send('This is contact')
+  res.render("contact")
 })
 
 app.listen(port,()=>{
